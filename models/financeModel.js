4ec776253e2c3745b352b1195d97b5b23e323fc7
Nguyen Nhat Thuong <thuongngnh@Nguyens-MacBook-Pro.local>
2023-12-31 02:13:09 +0700

    upload

---
+++ models/financeModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const financeSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    username: { type: String, default: "name" },
+    premium: {type: Boolean, default: false},
+    expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'expensesModel' }], 
+    incomes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'incomeModel' }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const FinanceUserModel = mongoose.model("financeuser", financeSchema);
+
+module.exports = FinanceUserModel;
